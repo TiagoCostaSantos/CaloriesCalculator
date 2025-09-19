8b2780bc4f8e8b2a25707752435cdc51f04ae6f9
Tiago <78939124+TiagoCostaSantos@users.noreply.github.com>
2025-09-19 13:19:53 -0300

    CRUD - C -> Cadastro dos produtos, falta tratamentos

---
+++ src/main/java/com/CaloriesCalculator/usecase/ProdutoAlimenticioUseCase.java
@@ -0,0 +1,8 @@
+package com.CaloriesCalculator.usecase;
+import com.CaloriesCalculator.model.ProdutoAlimenticioModel;
+
+public interface ProdutoAlimenticioUseCase {
+
+    void cadastrarProdutoAlimenticio(ProdutoAlimenticioModel produtoAlimenticioModel);
+
+}
