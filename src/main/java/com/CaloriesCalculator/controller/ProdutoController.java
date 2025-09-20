a530f32232eb2892402655a5b3336928ded0c6c1
Tiago <78939124+TiagoCostaSantos@users.noreply.github.com>
2025-09-19 23:02:23 -0300

    retirando trcorreção de erros

--- src/main/java/com/CaloriesCalculator/controller/ProdutoController.java
+++ src/main/java/com/CaloriesCalculator/controller/ProdutoController.java
@@ -1,5 +1,4 @@
 package com.CaloriesCalculator.controller;
-
 import com.CaloriesCalculator.model.ProdutoAlimenticioModel;
 import com.CaloriesCalculator.usecase.ProdutoAlimenticioUseCase;
 import org.springframework.stereotype.Controller;
@@ -30,15 +29,6 @@ public class ProdutoController {
 
     @PostMapping("/salvar")
     public String SalvarCadastroProdutoAlimenticio(@ModelAttribute("produtoAlimenticio") ProdutoAlimenticioModel produtoAlimenticio){
-        System.out.println("id" + produtoAlimenticio.getId());
-        System.out.println("titulo" + produtoAlimenticio.getTitulo());
-        System.out.println("tipo" + produtoAlimenticio.getTipo());
-        System.out.println("kcal" + produtoAlimenticio.getKcal());
-        System.out.println("carboidratos" + produtoAlimenticio.getCarboidratos());
-        System.out.println("proteinas" + produtoAlimenticio.getProteinas());
-        System.out.println("gordurasGerais" + produtoAlimenticio.getGorduraGerais());
-        System.out.println("peso" + produtoAlimenticio.getPeso());
-
         produtoAlimenticioUseCase.cadastrarProdutoAlimenticio(produtoAlimenticio);
         return "redirect:../?cadastro=sucesso";
     }
